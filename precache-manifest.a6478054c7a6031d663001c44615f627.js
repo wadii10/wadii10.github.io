f56f62dc914e07cbaa7336b54c4cbd04bd3e5004
wadii10 <wadii.driss@gmail.com>
2022-01-13 04:21:02 +0100

    thirdcommit

--- precache-manifest.a6478054c7a6031d663001c44615f627.js
+++ precache-manifest.a6478054c7a6031d663001c44615f627.js
@@ -1,42 +1,42 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "e584a6e4abd673459f0e26bb7a38184f",
-    "url": "/react-invoice-generator/index.html"
+    "url": "./index.html"
   },
   {
     "revision": "57393718eb245cd74da5",
-    "url": "/react-invoice-generator/static/css/2.0d77bc6e.chunk.css"
+    "url": "./static/css/2.0d77bc6e.chunk.css"
   },
   {
     "revision": "43e641daa055925ef94a",
-    "url": "/react-invoice-generator/static/css/main.3fe62ec5.chunk.css"
+    "url": "./static/css/main.3fe62ec5.chunk.css"
   },
   {
     "revision": "57393718eb245cd74da5",
-    "url": "/react-invoice-generator/static/js/2.37434c3d.chunk.js"
+    "url": "./static/js/2.37434c3d.chunk.js"
   },
   {
     "revision": "5fae47d0a016a5b68e2af829b08bec2a",
-    "url": "/react-invoice-generator/static/js/2.37434c3d.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.37434c3d.chunk.js.LICENSE.txt"
   },
   {
     "revision": "43e641daa055925ef94a",
-    "url": "/react-invoice-generator/static/js/main.9d2de3b6.chunk.js"
+    "url": "./static/js/main.9d2de3b6.chunk.js"
   },
   {
     "revision": "aabb303fe90f96d72bbb",
-    "url": "/react-invoice-generator/static/js/runtime-main.33c79cdc.js"
+    "url": "./static/js/runtime-main.33c79cdc.js"
   },
   {
     "revision": "5af81432dda1dde2def604f8052f424a",
-    "url": "/react-invoice-generator/static/media/download.5af81432.svg"
+    "url": "./static/media/download.5af81432.svg"
   },
   {
     "revision": "e631f0ca65031b20fe23668e166aa85b",
-    "url": "/react-invoice-generator/static/media/remove.e631f0ca.svg"
+    "url": "./static/media/remove.e631f0ca.svg"
   },
   {
     "revision": "a700735566a3701072afbd164b80e690",
-    "url": "/react-invoice-generator/static/media/resize.a7007355.svg"
+    "url": "./static/media/resize.a7007355.svg"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-invoice-generator/precache-manifest.a6478054c7a6031d663001c44615f627.js"
+  "./precache-manifest.a6478054c7a6031d663001c44615f627.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-invoice-generator/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
